b003ba75e7b90dc6cc24d1d87401e473cbf285df
Precious Olusola <preciousolusola16@gmail.com>
2022-12-08 15:58:36 -0800

    Added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints out a specific string
+ *
+ * Return: 1 as a delibrate output
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
