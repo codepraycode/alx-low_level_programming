77dedf739f18e40dc6f6df1c9df9e12baa100994
Precious Olusola <preciousolusola16@gmail.com>
2022-12-09 04:26:50 -0800

    Added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for program
+ *
+ * Return: 0 as a terminator
+ */
+int main(void)
+{
+	int b10;
+
+	for (b10 = 0; b10 < 10; b10++)
+	{
+		printf("%d", b10);
+	}
+
+	printf("\n");
+	return (0);
+}
