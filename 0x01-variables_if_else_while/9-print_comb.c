7b38d7a8d450c97a89fbb540f0a65daeb679c408
Precious Olusola <preciousolusola16@gmail.com>
2022-12-09 06:01:15 -0800

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for the program
+ *
+ * Return: 0 as a terminator
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
