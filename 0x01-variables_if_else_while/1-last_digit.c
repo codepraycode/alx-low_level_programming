5ee06213aa07766aee63629059426d68b7a1eaa2
Precious Olusola <preciousolusola16@gmail.com>
2022-12-09 03:55:40 -0800

    Fixed error in last submission

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 		printf("and is greater than 5");
 	if (ld == 0)
 		printf("and is 0");
-	if (ld < 6)
+	else if (ld < 6)
 		printf("and is less than 6 and not 0");
 	printf("\n");
 
