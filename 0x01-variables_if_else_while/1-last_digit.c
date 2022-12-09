89e38336fb6a0f6b69418b73d264d036c659a385
Precious Olusola <preciousolusola16@gmail.com>
2022-12-09 03:54:23 -0800

    Fixed error in last submission

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ int main(void)
 
 	printf("%d ", ld);
 
-	if (n > 5)
+	if (ld > 5)
 		printf("and is greater than 5");
-	if (n == 0)
+	if (ld == 0)
 		printf("and is 0");
-	if (n < 6)
+	if (ld < 6)
 		printf("and is less than 6 and not 0");
 	printf("\n");
 
