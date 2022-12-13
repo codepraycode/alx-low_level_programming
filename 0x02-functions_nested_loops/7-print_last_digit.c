08a915118f08c330124c16c90145e157cc1a263f
Precious Olusola <preciousolusola16@gmail.com>
2022-12-13 13:50:12 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of an interger
+ * @n: numeric value to operate on
+ *
+ * Return: int return value
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
