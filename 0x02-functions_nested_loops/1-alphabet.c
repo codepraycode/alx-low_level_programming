d338dcbd8e8dae49c2db90581bc34e466650282f
Precious Olusola <preciousolusola16@gmail.com>
2022-12-13 05:56:34 -0800

    Removed the entry point from last submission

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,14 +14,3 @@ void print_alphabet(void)
 
 	_putchar('\n');
 }
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
