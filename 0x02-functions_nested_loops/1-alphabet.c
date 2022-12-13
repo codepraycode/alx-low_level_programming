a267e104e2b183d174841607c031c86346a02031
Precious Olusola <preciousolusola16@gmail.com>
2022-12-13 05:55:04 -0800

    Fixed error in last submission

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,5 +12,16 @@ void print_alphabet(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 
-	putchar('\n');
+	_putchar('\n');
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
