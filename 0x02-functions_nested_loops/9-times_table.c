18c0dfc09b7730aade896d91e050db640d7d347d
Precious Olusola <preciousolusola16@gmail.com>
2022-12-13 14:35:56 -0800

    Added te times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times time table starting from 0
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	for (int row = 0; row <= 9; row++)
+	{
+		for (int col = 0; col <= 9; col++)
+		{
+			int mul = row * col;
+
+			if (col > 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+			}
+
+			_putchar(mul + '0');
+
+			if (col < 9)
+				_putchar(',');
+
+		}
+	}
+}
