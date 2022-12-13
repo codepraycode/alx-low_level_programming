30cd2a38148bb5c8eee56f1e3da19581b6fd4ae6
Precious Olusola <preciousolusola16@gmail.com>
2022-12-13 14:58:20 -0800

    Added 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Add up two given integerss
+ * @n1: first integer
+ * @n2: seconds integer
+ *
+ * Return: sum of the two given integers
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
