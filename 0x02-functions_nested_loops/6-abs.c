9381ce9e42bcd8cfff6df8f50b242b7814fbdb25
Precious Olusola <preciousolusola16@gmail.com>
2022-12-13 13:44:35 -0800

    Done with 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ * @n: numeric value to check
+ *
+ * Return: int return value
+ */
+int _abs(int n)
+{
+
+	if (n > 0)
+		return (n * -1);
+
+	return (n);
+}
