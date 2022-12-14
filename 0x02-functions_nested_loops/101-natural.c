861c20537ff89b6ee3fa61963a341c306521e239
Precious Olusola <preciousolusola16@gmail.com>
2022-12-13 16:52:06 -0800

    Added 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int sum, i;
+
+	sum = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3) == 0)
+			sum += i
+		else if ((i % 5) == 0)
+			sum += i
+	}
+
+
+	printf("%d\n", sum);
+	return (0);
+
+}
