0d7d3ac469c956ceede601c177347ba8b1a44b6d
Precious Olusola <preciousolusola16@gmail.com>
2022-12-13 16:24:31 -0800

    Added 100-times_table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * print_digits - Print digits correctly
+ * @n: the number to print
+ *
+ * Return: void, prints out the outputs
+ */
+void print_digits(int n)
+{
+
+	int isNegative = n < 0 ? 1 : 0;
+
+	if (isNegative)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	if (n > 99)
+	{
+		int rem = n % 100;
+
+		_putchar((n / 100) + '0');
+		_putchar((rem / 10) + '0');
+		_putchar((rem % 10) + '0');
+	}
+	else if ((n > 9))
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+		_putchar(n + '0');
+}
+
+/**
+ * print_times_table - prints n times time table starting from 0
+ * @n: The number to stop in table
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int row, col;
+
+	for (row = 0; row <= n; row++)
+	{
+		for (col = 0; col <= n; col++)
+		{
+			int mul = row * col;
+
+			print_digits(mul);
+
+			if (col < n)
+				_putchar(',');
+
+		}
+		_putchar('\n');
+	}
+}
