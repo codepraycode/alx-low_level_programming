29ddb351dc719f1d33d624ec5fce3745411ffe8c
Precious Olusola <preciousolusola16@gmail.com>
2022-12-13 16:11:18 -0800

    Optimized solution

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,40 @@
 #include "main.h"
 
 /**
+ * print_digits - Print digits correctly
+ * @n: the number to print
+ *
+ * Return: void, prints out the outputs
+ */
+void print_digits(int n)
+{
+
+	int isNegative = n < 0 ? 1 : 0;
+
+	if (isNegative)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	if (n > 99)
+	{
+		int rem = n % 100;
+
+		_putchar((n / 100) + '0');
+		_putchar((rem / 10) + '0');
+		_putchar((rem % 10) + '0');
+	}
+	else if ((n > 9))
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+		_putchar(n + '0');
+}
+
+/**
  * print_to_98 - Print either to or fro to 98
  * @n: starting number
  *
@@ -8,24 +42,11 @@
  */
 void print_to_98(int n)
 {
+
 	for (;;)
 	{
 
-		if (n > 99)
-		{
-			int rem = n % 100;
-
-			_putchar((n / 100) + '0');
-			_putchar((rem / 10) + '0');
-			_putchar((rem % 10) + '0');
-		}
-		else if ((n > 9))
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-		}
-		else
-			_putchar(n + '0');
+		print_digits(n);
 
 		if (n > 98)
 			n -= 1;
