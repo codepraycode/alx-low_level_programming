ee71d2eb5f54f4c7425a59cd28cd8a65afdeb742
Precious Olusola <preciousolusola16@gmail.com>
2022-12-13 16:29:02 -0800

    Added 100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,17 +21,25 @@ void print_digits(int n)
 	{
 		int rem = n % 100;
 
+		_putchar(' ');
 		_putchar((n / 100) + '0');
 		_putchar((rem / 10) + '0');
 		_putchar((rem % 10) + '0');
 	}
 	else if ((n > 9))
 	{
+		_putchar(' ');
+		_putchar(' ');
 		_putchar((n / 10) + '0');
 		_putchar((n % 10) + '0');
 	}
 	else
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
 		_putchar(n + '0');
+	}
 }
 
 /**
