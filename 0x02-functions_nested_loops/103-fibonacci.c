9c32461c1229df68e1d0ddb36c71893c7c0f2e79
Precious Olusola <preciousolusola16@gmail.com>
2022-12-13 17:30:06 -0800

    Added 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: void
+ */
+int main(void)
+{
+	long int sum, d, a, b;
+
+	a = 1;
+	b = 2;
+
+	for (;;)
+	{
+		d = a + b;
+
+		a = b;
+		b = d;
+
+		if (d > 4000000)
+			break;
+
+		if ((d % 2) == 0)
+			sum += d;
+	}
+
+
+	printf("%lu\n", sum);
+	return (0);
+
+}
