392e59c745784e410a13c1546147d80317a706b9
Precious Olusola <preciousolusola16@gmail.com>
2022-12-16 04:26:10 -0800

    Added 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the FizzBuzz program
+ *
+ * Return: 0 to terminate
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+			printf("Fizz");
+		else if ((i % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
