3d693ae0f45f605d327fb0b7fa9452ee150d6385
Precious Olusola <preciousolusola16@gmail.com>
2022-12-16 02:53:42 -0800

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * mul - entry point for the program to check for digit 0 - 9
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
