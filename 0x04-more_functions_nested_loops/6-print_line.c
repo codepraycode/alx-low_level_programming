ad8d6513a927d982e084190aa3fda8b810829a14
Precious Olusola <preciousolusola16@gmail.com>
2022-12-16 03:45:58 -0800

    Added 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Draw a straigth line in terminal
+ * @n: length of line
+ *
+ * Return: void (nothing)
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
