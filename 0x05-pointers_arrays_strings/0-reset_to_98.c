ef741dc4648294c0606418bf8d0e70b5c7fa44c8
Precious Olusola <preciousolusola16@gmail.com>
2022-12-19 22:53:14 -0800

    Added 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - Function that takes a pointer to an int and update it points to 98
+ * @n: pointer address
+ *
+ * Return: void (nothing)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
