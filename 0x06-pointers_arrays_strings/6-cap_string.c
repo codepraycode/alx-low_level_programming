e4aeec842bfb2f4514ce07a75b7e00f5306d4082
Precious Olusola <preciousolusola16@gmail.com>
2022-12-22 08:40:56 -0800

    Updated Last submission

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,35 +8,32 @@
  */
 char *cap_string(char *str)
 {
-	int i, capitalization_flag;
-
-	capitalization_flag = 1;
+	int i, ii, capitalization_flag = 1;
+	char ch[13] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\t', '\n', ' '};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n'))
-			capitalization_flag = 1;
 
-		else if ((str[i] == ',') || (str[i] == ';') || (str[i] == '.'))
-			capitalization_flag = 1;
-
-		else if ((str[i] == '!') || (str[i] == '?') || (str[i] == '"'))
-			capitalization_flag = 1;
-
-		else if ((str[i] == '(') || (str[i] == ')'))
-			capitalization_flag = 1;
-
-		else if ((str[i] == '{') || (str[i] == '}'))
-			capitalization_flag = 1;
-
+		if ((str[i] >= 'A') && (str[i] <= 'Z'))
+			capitalization_flag = 0;
+		else if ((str[i] >= '0') && (str[i] <= '9'))
+			capitalization_flag = 0;
 		else if (capitalization_flag && (str[i] >= 'a') && (str[i] <= 'z'))
 		{
 			str[i] = str[i] - 32;
 			capitalization_flag = 0;
 		}
-		else if ((str[i] >= 'A') && (str[i] <= 'Z'))
-			capitalization_flag = 0;
+		else
+		{
+			for (ii = 0; ii < 13; ii++)
+			{
+				if (ch[ii] == str[i])
+				{
+					capitalization_flag = 1;
+					break;
+				}
+			}
+		}
 	}
-
 	return (str);
 }
