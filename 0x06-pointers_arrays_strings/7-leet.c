6b8d1e7a612dd59cdb693ba212f1c4097c6ad3a4
Precious Olusola <preciousolusola16@gmail.com>
2022-12-22 20:16:21 -0800

    Updated last submission

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,17 +12,20 @@ char *leet(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
+		if (1)
+		{
 
-		if ((str[i] == 'a') || (str[i] == 'A'))
-			str[i] = '4';
-		else if ((str[i] == 'e') || (str[i] == 'E'))
-			str[i] = '3';
-		else if ((str[i] == 'o') || (str[i] == 'O'))
-			str[i] = '0';
-		else if ((str[i] == 't') || (str[i] == 'T'))
-			str[i] = '7';
-		else if ((str[i] == 'l') || (str[i] == 'L'))
-			str[i] = '1';
+			if ((str[i] == 'a') || (str[i] == 'A'))
+				str[i] = '4';
+			else if ((str[i] == 'e') || (str[i] == 'E'))
+				str[i] = '3';
+			else if ((str[i] == 'o') || (str[i] == 'O'))
+				str[i] = '0';
+			else if ((str[i] == 't') || (str[i] == 'T'))
+				str[i] = '7';
+			else if ((str[i] == 'l') || (str[i] == 'L'))
+				str[i] = '1';
+		}
 	}
 	return (str);
 }
