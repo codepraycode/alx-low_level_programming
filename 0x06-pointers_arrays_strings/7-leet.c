0047256ebdc7ceb2e7a94f9b59f8ec1e2828ba2b
Precious Olusola <preciousolusola16@gmail.com>
2022-12-22 20:09:30 -0800

    Added 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * leet - Encodes string into 1337
+ * @str: a pointer to a string to encode
+ *
+ * Return: a string character encoded
+ */
+char *leet(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+
+		if ((str[i] == 'a') || (str[i] == 'A'))
+			str[i] = '4';
+		else if ((str[i] == 'e') || (str[i] == 'E'))
+			str[i] = '3';
+		else if ((str[i] == 'o') || (str[i] == 'O'))
+			str[i] = '0';
+		else if ((str[i] == 't') || (str[i] == 'T'))
+			str[i] = '7';
+		else if ((str[i] == 'l') || (str[i] == 'L'))
+			str[i] = '1';
+	}
+	return (str);
+}
