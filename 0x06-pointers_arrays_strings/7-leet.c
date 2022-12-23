d826479c89dead1700e2e5de2bc43ff48ca58751
Precious Olusola <preciousolusola16@gmail.com>
2022-12-22 20:22:46 -0800

    Updated last submission

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,23 +8,21 @@
  */
 char *leet(char *str)
 {
-	int i;
+	int i, j;
+	char chsm[5] = {'a', 'e', 'o', 't', 'l'};
+	char chbm[5] = {'A', 'E', 'O', 'T', 'L'};
+	char val[5] = {'4', '3', '0', '7', '1'};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (1)
+		for (j = 0; j < 5; j++)
 		{
 
-			if ((str[i] == 'a') || (str[i] == 'A'))
-				str[i] = '4';
-			else if ((str[i] == 'e') || (str[i] == 'E'))
-				str[i] = '3';
-			else if ((str[i] == 'o') || (str[i] == 'O'))
-				str[i] = '0';
-			else if ((str[i] == 't') || (str[i] == 'T'))
-				str[i] = '7';
-			else if ((str[i] == 'l') || (str[i] == 'L'))
-				str[i] = '1';
+			if ((str[i] == chsm[j]) || (str[i] == chbm[j]))
+			{
+				str[i] = val[j];
+				break;
+			}
 		}
 	}
 	return (str);
