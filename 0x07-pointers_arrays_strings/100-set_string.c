96d46d50b1cf70148bb9a5c2244862a98596c41e
Precious Olusola <preciousolusola16@gmail.com>
2023-01-03 16:09:33 -0800

    Added the first advance task in project

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+/**
+ * set_string - A function that sets the value of a pointer to a char
+ * @s: string pointer
+ * @to: char to set pointer value to
+ *
+ * Return: void, nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
