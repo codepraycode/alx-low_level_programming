c2545e2cf06255e2728f514fcef1b7058f7f2cc6
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-01-06 09:03:46 +0100

    Added _puts

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * _puts - takes pointer to a string and prints the string
+ * @str: string to print
+ *
+ * Return: void (nothing)
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
